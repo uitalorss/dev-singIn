67b550da8d4b66b702cd29d2aaebf4b1d372a3de
Uítalo <uitalo.urss@gmail.com>
2024-01-19 17:11:20 -0300

    criação dos recursos user e auth e configuração de banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
